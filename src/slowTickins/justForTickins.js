4d54aa9d3f540a760adea60af155e52cc786cf36
ebenpack <ebenpackwood@gmail.com>
2019-09-27 21:40:18 -0400

    fix stuff i guess

--- src/slowTickins/justForTickins.js
+++ src/slowTickins/justForTickins.js
@@ -2,7 +2,7 @@ import { doALittleTick } from './tickinsModule';
 
 export const tickADiddlyAMiddlewareARooni = (store) => (next) => (action) => {
 	const gameSpeed = store.getState().tickins.gameSpeed;
-	const gameOver = store.getState().appModule.gameOver;
+	const gameOver = store.getState().appModule;
 	if (gameSpeed && !gameOver) {
 		window.setTimeout(() => store.dispatch(doALittleTick()), gameSpeed);
 	}
