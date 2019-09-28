30722591298a3c6fda7b08f983a39b06f68676d3
Derek Dorr <derek.dorr@gmail.com>
2019-09-27 21:35:47 -0400

    Add buildItem action to buildDialogModule.js

--- src/buildDialog/buildDialogModule.js
+++ src/buildDialog/buildDialogModule.js
@@ -1,7 +1,12 @@
 import { RESTART_GAME } from '../app/appModule';
 
+export const BUILD_ITEM = 'BUILD_ITEM';
 const CLOSE_BUILD_DIALOG = 'CLOSE_BUILD_DIALOG';
 const OPEN_BUILD_DIALOG = 'OPEN_BUILD_DIALOG';
+
+export const buildItem = (cost, task) => (dispatch) => dispatch({ type: BUILD_ITEM, data: { cost, task } });
+export const closeBuildDialog = () => (dispatch) => dispatch({ type: CLOSE_BUILD_DIALOG });
+export const openBuildDialog = () => (dispatch) => dispatch({ type: OPEN_BUILD_DIALOG });
 
 const buildModuleReducer = (state = false, { type }) => {
 	switch (type) {
