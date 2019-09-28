b5b8cde7881b0208b67c2229ea51fe2f5ec86593
Derek Dorr <derek.dorr@gmail.com>
2019-09-27 20:40:34 -0400

    Restructure actions / reducer

--- src/actions/GameConditionsActions.js
+++
@@ -1,5 +0,0 @@
-export const RESTART_GAME = 'RESTART_GAME';
-export const END_GAME = 'END_GAME';
-
-export const restartGame = () => dispatch => dispatch({ type: RESTART_GAME });
-export const endGame = () => dispatch => dispatch({ type: END_GAME });
