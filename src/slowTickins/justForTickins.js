01a3e5f8e1de6f7c8a59fadd90546911dee63762
ebenpack <ebenpackwood@gmail.com>
2019-09-27 22:14:06 -0400

    enhance your calm

--- src/slowTickins/justForTickins.js
+++ src/slowTickins/justForTickins.js
@@ -1,10 +1,12 @@
 import { doALittleTick } from './tickinsModule';
 
+const GAME_CHILL_LEVEL_BASIS = 500;
+
 export const tickADiddlyAMiddlewareARooni = (store) => (next) => (action) => {
 	const gameSpeed = store.getState().gameSpeed;
 	const gameOver = store.getState().appModule;
 	if (gameSpeed && !gameOver) {
-		window.setTimeout(() => store.dispatch(doALittleTick()), gameSpeed);
+		window.setTimeout(() => store.dispatch(doALittleTick()), gameSpeed * GAME_CHILL_LEVEL_BASIS);
 	}
 	return next(action);
 };
