463a2c1d839ec74dfe13a7bbb3d113d8cc714e50
ebenpack <ebenpackwood@gmail.com>
2019-09-27 21:17:35 -0400

    add game notQuiteMyTempo

--- src/slowTickins/tickinsModule.js
+++ src/slowTickins/tickinsModule.js
@@ -3,6 +3,8 @@ export const CHANGE_GAME_SPEED = 'MOAR_TICKINS';
 
 export const doALittleTick = () => ({ type: TICK });
 
+export const notQuiteMyTempo = (thatsMyTempo) => ({ type: CHANGE_GAME_SPEED, tempo: thatsMyTempo });
+
 const initial = Object.freeze({
     gameSpeed: 1000
 });
@@ -10,7 +12,7 @@ const initial = Object.freeze({
 export const tickinsReducer = (store=initial, action) => {
     switch (action.type) {
         case CHANGE_GAME_SPEED:
-            return {...store, gameSpeed: action.gameSpeed};
+            return {...store, gameSpeed: action.tempo};
         default:
             return store;
     }
