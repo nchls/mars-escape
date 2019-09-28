a800171984dcdae688e5e709127fa8a7467ba065
ebenpack <ebenpackwood@gmail.com>
2019-09-28 16:44:46 -0400

    refactor storage stuff

--- src/endGame/endGameModule.js
+++ src/endGame/endGameModule.js
@@ -1,34 +1,23 @@
 import { RESTART_GAME, START_APP } from '../app/appModule';
 import { TICK } from '../gameTicks/gameTicksModule';
 
+import { setStorage, getStorage } from '../util/storage';
+
 const SAVE_BEST_TIMES = 'SAVE_BEST_TIMES';
 const STORAGE_KEY = 'BEST_TIMES';
 
 const initialState = Object.freeze([]);
 
-const setStorage = (data) => {
-	try {
-		localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
-		return data;
-	} catch (e) {
-		return initialState;
-	}
-};
+const saveBestTime = setStorage(STORAGE_KEY)(initialState);
 
-const getStorage = () => {
-	try {
-		return JSON.parse(localStorage.getItem(STORAGE_KEY));
-	} catch (e) {
-		return initialState;
-	}
-};
+const getBestTime = getStorage(STORAGE_KEY)(initialState);
 
 export const saveBestTimes = (data) => (dispatch) => dispatch({ type: SAVE_BEST_TIMES, data });
 
 export const bestTimesReducer = (state = initialState, { type, data }) => {
 	switch (type) {
 	case START_APP:
-		return getStorage();
+		return getBestTime();
 	case SAVE_BEST_TIMES: {
 		const date = new Date().toDateString();
 		const time = data / 100;
@@ -36,7 +25,7 @@ export const bestTimesReducer = (state = initialState, { type, data }) => {
 			.sort((a, b) => (a.time < b.time ? -1 : 1))
 			.slice(0, 5);
 		console.log({ date, time, bestTimes });
-		return setStorage(bestTimes);
+		return saveBestTime(bestTimes);
 	}
 	default:
 		return state;
