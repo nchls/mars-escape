a93c5cc7d6b23e363ef3ccae2526ff56a23917ee
Nolan Tanguay <nolan.tanguay@cashstar.com>
2019-09-28 12:51:28 -0400

    Change Game Speed

--- src/gameSpeed/GameSpeed.js
+++ src/gameSpeed/GameSpeed.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import {changeGameSpeed} from '../gameTicks/gameTicksModule';
+
 import './gameSpeed.scss';
 
+const GameSpeed = ({ gameSpeed, changeGameSpeed }) => {
 
-const GameSpeed = ({ }) => {
+	const onChange = (event) => {
+		changeGameSpeed(parseInt(event.target.value));
+	};
+
 	return (
-		<p>Game speed</p>
+		<p className='game-speed'>
+			<span> Game speed: </span>
+			<select id="leave" onChange={onChange} value={gameSpeed}>
+				<option value='0'>Paused</option>
+				<option value='1'>Normal</option>
+				<option value='2'>Fast</option>
+				<option value='3'>Relativistic</option>
+			</select>
+		</p>
 	);
 };
 
-const mapStateToProps = (state) => {
-	return {
+const mapStateToProps = state => ({
+	gameSpeed: state.gameSpeed
+});
 
-	};
-};
+const mapDispatchToProps = dispatch => ({
+	changeGameSpeed: speed => dispatch(changeGameSpeed(speed))
+});
 
-export default connect(mapStateToProps)(GameSpeed);
+export default connect(mapStateToProps, mapDispatchToProps)(GameSpeed);
