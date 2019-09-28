b9cdae5f2d3401585f11c5c712da348c5fb73d03
Derek Dorr <derek.dorr@gmail.com>
2019-09-28 15:46:21 -0400

    Fix pause bug

--- src/gameTicks/tickMiddleware.js
+++ src/gameTicks/tickMiddleware.js
@@ -8,12 +8,17 @@ const GAME_CHILL_LEVEL_BASIS = {
 };
 
 export const tickADiddlyAMiddlewareARooni = (store) => (next) => (action) => {
+	const gameSpeed = store.getState().gameSpeed;
 	if (action.type === TICK || action.type === START_APP) {
-		const gameSpeed = store.getState().gameSpeed;
 		const gameOver = store.getState().gameOver;
 		if (gameSpeed && !gameOver) {
 			window.setTimeout(() => store.dispatch(nextTick()), GAME_CHILL_LEVEL_BASIS[gameSpeed]);
 		}
+	}
+
+	// This feels wrong
+	if (action.type === CHANGE_GAME_SPEED && gameSpeed === 0 && action.gameSpeed) {
+		window.setTimeout(() => store.dispatch(nextTick()), GAME_CHILL_LEVEL_BASIS[action.gameSpeed]);
 	}
 	return next(action);
 };
