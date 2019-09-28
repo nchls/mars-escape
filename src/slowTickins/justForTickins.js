fe4bef49e84c14c96e322d1df390e36209662688
ebenpack <ebenpackwood@gmail.com>
2019-09-27 21:15:05 -0400

    no gameover ticks

--- src/slowTickins/justForTickins.js
+++ src/slowTickins/justForTickins.js
@@ -2,6 +2,9 @@ import { doALittleTick } from './tickinsModule';
 
 export const tickADiddlyAMiddlewareARooni = store => next => action => {
     const gameSpeed = store.getState().tickins.gameSpeed;
-    gameSpeed && window.setTimeout(() => store.dispatch(doALittleTick()), gameSpeed)
+    const gameOver = store.getState().appModule.gameOver;
+    if (gameSpeed && !gameOver) {
+        window.setTimeout(() => store.dispatch(doALittleTick()), gameSpeed)
+    }
     return next(action)
 }
