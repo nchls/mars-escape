2bff12cc03a63abe03b1981c8febb4350409a769
ebenpack <ebenpackwood@gmail.com>
2019-09-27 20:28:32 -0400

    add store

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
+import store from './store'
 
 import './app.scss';
 
 
 const App = () => {
 	return (
-		<div className="app">
-			Hello world
-		</div>
+		<Provider store={store}>
+			<div className="app">
+				Hello world
+			</div>
+		</Provider>
 	);
 };
 
---
+++ src/app/store.js
@@ -0,0 +1,13 @@
+import { combineReducers, createStore, applyMiddleware, compose } from 'redux';
+
+const reducers = combineReducers({
+});
+// eslint-disable-next-line no-underscore-dangle
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middleware = [];
+const store = createStore(
+    reducers,
+    composeEnhancers(applyMiddleware(...middleware)),
+);
+
+export default store;
