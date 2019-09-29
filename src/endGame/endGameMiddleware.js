591d78cf9a58cc867b1ecdf936a8158ec5c37360
Derek Dorr <derek.dorr@gmail.com>
2019-09-29 08:17:45 -0400

    If we have a functioning rove with winch and no ore, game should continue

--- src/endGame/endGameMiddleware.js
+++ src/endGame/endGameMiddleware.js
@@ -13,13 +13,25 @@ const endGameMiddleware = (store) => (next) => (action) => {
 
 	if (!gameOver && ore < 350) {
 		const { length } = rovers;
-		const disabledRovers = rovers.filter(({ status }) => [LOST, STUCK, OUT_OF_POWER, FALLEN_OFF_CLIFF].includes(status));
+		const stuckRovers = rovers.filter(({ status }) => [STUCK, OUT_OF_POWER, LOST].includes(status));
+		const disabledRovers = rovers.filter(({ status }) => [FALLEN_OFF_CLIFF].includes(status));
 		if (tasks.length === 0) {
-			const sellCost = inventory.filter((item) => item.cost && item.itemId !== 22).reduce((acc, item) => acc + Math.floor(item.cost / 2), 0);
+			const sellCost = inventory
+				.filter((item) => item.cost && item.itemId !== 22)
+				.reduce((acc, item) => acc + Math.floor(item.cost / 2), 0);
 			const hasWinch = inventory.find((item) => item.itemId === 22) ? 150 : 0;
 			const finalOre = sellCost + hasWinch + ore;
-			if ((length === disabledRovers.length && finalOre < 350) || finalOre < 150) {
-				setTimeout(() => store.dispatch(endGame()), 0);
+			if (
+				(length === disabledRovers.length)
+				|| (length === (disabledRovers.length + stuckRovers.length) && finalOre < 350)
+				|| finalOre < 150
+			) {
+				const functioningRoversWithWinches = rovers
+					.filter(({ modules, status }) => ![STUCK, OUT_OF_POWER, LOST, FALLEN_OFF_CLIFF].includes(status) && modules.includes(22));
+
+				if (functioningRoversWithWinches.length === 0) {
+					setTimeout(() => store.dispatch(endGame()), 0);
+				}
 			}
 		}
 	}
